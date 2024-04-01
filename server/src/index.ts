6d6734f6bbe3dce260816f3b44ea44b0d17cf2dd
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2024-04-01 20:46:41 +0200

    Team id generation

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,31 @@ import {Server} from 'socket.io';
 
 const io = new Server({});
 
+const teams: string[] = [];
+
+const doesContainTeamId = (id: string) => teams.includes(id);
+
+const addTeamId = (id: string) => teams.push(id);
+
+const generateNewTeamId = () => {
+  let id: string;
+  do {
+    id = Math.random().toString().slice(2, 10);
+  } while (doesContainTeamId(id));
+  addTeamId(id);
+  return id;
+};
+
 io.on('connection', socket => {
   console.log('Hello World!');
+  socket.on('createTeam', () => {
+    if (typeof socket.data.teamId === 'string') return;
+    const id = generateNewTeamId();
+    socket.data = {teamId: id};
+    socket.join(id);
+    io.to(id).emit('data', {teamId: id});
+    console.log(id);
+  });
 });
 
 io.listen(Number(process.env.PORT));
