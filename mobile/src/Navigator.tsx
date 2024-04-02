9b1ba5e2b55f8e27d7e1f67b2c41e74d4a600125
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2024-04-02 23:09:33 +0200

    State based navigation

--- mobile/src/Navigator.tsx
+++ mobile/src/Navigator.tsx
@@ -3,18 +3,34 @@ import { ThemeContext } from "./Theme";
 import { useContext } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import JoinTeam from "./JoinTeam";
+import useStore from "./useStore";
+import { Text } from "react-native-paper";
 
 const Stack = createNativeStackNavigator();
 
 const Navigator = () => {
   const { theme } = useContext(ThemeContext);
+  const team = useStore(state => state.team);
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name="join"
-          component={JoinTeam}
-        />
+        {
+          team ? (
+            <>
+              <Stack.Screen
+                name="home"
+                component={()=>(<Text>Hello World!</Text>)}
+              />
+            </>
+          ) : (
+            <>
+              <Stack.Screen
+                name="join"
+                component={JoinTeam}
+              />
+            </>
+          )
+        }
       </Stack.Navigator>
     </NavigationContainer>
   );
