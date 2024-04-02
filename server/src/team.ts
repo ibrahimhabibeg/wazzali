4c4cfa0b1912b1df74518a141eafcf776152340f
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2024-04-02 22:42:56 +0200

    Update team code number of letters

--- server/src/team.ts
+++ server/src/team.ts
@@ -3,7 +3,7 @@ import {doesContainTeamCode} from './persistence';
 const generateNewTeamCode = () => {
   let code: string;
   do {
-    code = Math.random().toString().slice(2, 10);
+    code = Math.random().toString().slice(2, 8);
   } while (doesContainTeamCode(code));
   return code;
 };
