9a31ee5767af94bcfe3960749ae3e30c64c6e187
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2024-04-03 16:04:29 +0200

    Fix bug in persistance

--- server/src/persistence.ts
+++ server/src/persistence.ts
@@ -1,10 +1,11 @@
 import {Team} from './index';
 
-const teams: Team[] = [];
+let teams: Team[] = [];
 
 export const doesContainTeamCode = (code: string) =>
   teams.reduce((acc, team) => team.code === code || acc, false);
-export const storeTeam = (team: Team) => teams.push(team);
+export const storeTeam = (newTeam: Team) =>
+  (teams = [...teams.filter(team => team.code !== newTeam.code), newTeam]);
 
 export const getTeam = (code: string) => {
   const filteredTeams = teams.filter(team => team.code === code);
