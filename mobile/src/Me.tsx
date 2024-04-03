e45b1fc0f6bda36a782934f3ea334d4be4436061
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2024-04-03 16:49:19 +0200

    Add me section in home page

---
+++ mobile/src/Me.tsx
@@ -0,0 +1,27 @@
+import { Pressable, View } from "react-native";
+import { Avatar, Icon, Text } from "react-native-paper";
+import { useContext, useState } from "react";
+import { ThemeContext } from "./Theme";
+import useStore from "./useStore";
+import EditMyDataModal from "./EditMyDataModal";
+
+const Me = () => {
+  const me = useStore(state => state.me);
+  const { theme } = useContext(ThemeContext);
+  const [isEditing, setIsEditing] = useState(false);
+  return (
+    <>
+      <View style={{ alignItems: "center", width: "85%", marginTop: 20, minHeight: 200, borderWidth:1, borderRadius: 10, borderColor:theme.colors.border, paddingVertical:10, paddingHorizontal:15 }}>
+          <Pressable style={{ alignSelf: "flex-end" }} onPress={() => setIsEditing(val => !val)}>
+            <Icon size={22} source={"square-edit-outline"} />
+          </Pressable>
+          <Avatar.Icon icon={"folder"} size={50} />
+          <Text variant={"headlineSmall"} style={{ marginTop: 10, textAlign:"center" }}>{me?.username}</Text>
+          <Text style={{ textAlign: "center", marginTop: 20, marginBottom: 30 }}>{me?.description}</Text>
+      </View>
+      <EditMyDataModal visible={isEditing} hide={()=>setIsEditing(false)}/>
+    </>
+  );
+};
+
+export default Me;
