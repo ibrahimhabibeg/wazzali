4ecc62d4911aa3c450c404273d31781d13d4c671
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2024-04-04 02:30:46 +0200

    Fix Bug

--- server/src/editMyData.ts
+++ server/src/editMyData.ts
@@ -7,14 +7,18 @@ const editMyData =
     io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
     socket: Socket
   ) =>
-  ({username: newUsername, description: newDescription}: UserData) => {
+  async ({username: newUsername, description: newDescription}: UserData) => {
     if (!newUsername) return;
     const {teamCode} = socket.data;
     if (typeof teamCode !== 'string') return;
-    const oldTeam = getTeam(teamCode);
+    const oldTeam = await getTeam(teamCode);
     if (!oldTeam) return;
     const oldUsers = oldTeam.users;
-    if (oldUsers.some(user => user.username === newUsername)) return;
+    if (
+      newUsername !== socket.data.username &&
+      oldUsers.some(user => user.username === newUsername)
+    )
+      return;
     const oldUser = oldUsers.find(
       user => user.username === socket.data.username
     );
@@ -24,14 +28,17 @@ const editMyData =
       username: newUsername,
       description: newDescription,
     };
-    const newUsers = [...oldTeam.users, newUser].filter(
-      user => user.username !== oldUser.username
-    );
+    const newUsers = [
+      ...oldTeam.users.filter(user => user.username !== oldUser.username),
+      newUser,
+    ];
+    console.log(newUsers);
     const newTeam = {
       ...oldTeam,
       users: newUsers,
     };
     storeTeam(newTeam);
+    socket.data.username = newUser.username;
     io.to(teamCode).emit('data', newTeam);
     socket.emit('me', newUser);
   };
